3b3e50a562182ca3b1cccae500d5dd8807f23f88
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-10-29 14:31:27 -0400

    Last Commit before it was working

--- Blood-Princess/Assets/Scripts/PCG/MapManager.cs
+++ Blood-Princess/Assets/Scripts/PCG/MapManager.cs
@@ -5,18 +5,18 @@ using PCG;
 
 public class MapManager : MonoBehaviour
 {
-	public IntVector2 BoardDimension = new IntVector2(4, 4);
-	public int Length;
-	public GameObject Zone;
-	// Start is called before the first frame update
-	void Awake()
-	{
-		int seed = Random.Range(0, 100000);
-		Debug.Log(seed);
-		// Set Zone according to the board Dimension
-		Zone.GetComponent<BoxCollider>().size = new Vector3(BoardDimension.x * Utility.TileSize().x, BoardDimension.y * Utility.TileSize().y);
-		Zone.GetComponent<BoxCollider>().center = new Vector3(BoardDimension.x * Utility.TileSize().x, BoardDimension.y * Utility.TileSize().y) / 2f;
-		Board board = new Board(BoardDimension.x, BoardDimension.y, seed, Length);
-		//Utility.MaxFileHeight();
-	}
+    public IntVector2 BoardDimension = new IntVector2(4, 4);
+    public int Length;
+    public GameObject Zone;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int seed = Random.Range(0, 100000);
+        Debug.Log(seed);
+        // Set Zone according to the board Dimension
+        Zone.GetComponent<BoxCollider>().size = new Vector3(BoardDimension.x * PCG.Utility.TileSize().x, BoardDimension.y * PCG.Utility.TileSize().y);
+        Zone.GetComponent<BoxCollider>().center = new Vector3(BoardDimension.x * PCG.Utility.TileSize().x, BoardDimension.y * PCG.Utility.TileSize().y) / 2f;
+        Board board = new Board(BoardDimension.x, BoardDimension.y, seed, Length);
+        //Utility.MaxFileHeight();
+    }
 }
