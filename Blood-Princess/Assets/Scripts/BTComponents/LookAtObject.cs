0320104d8f7e0aee4af5a0bf7b6ef280ef24f6dd
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-11-17 01:38:25 -0500

    Update block

--- Blood-Princess/Assets/Scripts/BTComponents/LookAtObject.cs
+++ Blood-Princess/Assets/Scripts/BTComponents/LookAtObject.cs
@@ -7,34 +7,46 @@ using BehaviorDesigner.Runtime.Tasks;
 [RequiredComponent(typeof(SpeedManager))]
 public class LookAtObject : Action
 {
-    public SharedGameObject Target;
-    private SpeedManager m_SpeedManager;
-    private SpeedManager m_PlayerSpeedManager;
+	public SharedGameObject Target;
+	public SharedFloat Duration;
 
-    public override void OnAwake()
-    {
-        if (Target == null || Target.Value == null)
-        {
-            Target.Value = GameObject.FindGameObjectWithTag("Player");
-            Debug.Assert(Target.Value != null, "Could not find Player Object");
-        }
-        m_SpeedManager = Owner.GetComponent<SpeedManager>();
-        Debug.Assert(m_SpeedManager != null, "Could not find speed manager");
-        m_PlayerSpeedManager = Target.Value.GetComponent<SpeedManager>();
-        Debug.Assert(m_PlayerSpeedManager != null, "Could not find speed manager on player");
-    }
+	private SpeedManager m_SpeedManager;
+	private SpeedManager m_PlayerSpeedManager;
+	private float m_Timer;
 
-    public override TaskStatus OnUpdate()
-    {
-        if (m_SpeedManager.GetTruePos().x > m_PlayerSpeedManager.GetTruePos().x)
-        {
-            Owner.transform.eulerAngles = new Vector3(0f, 180f, 0f);
-        }
-        else
-        {
-            Owner.transform.eulerAngles = new Vector3(0f, 0f, 0f);
-        }
-        return TaskStatus.Success;
-    }
+	public override void OnAwake()
+	{
+		if (Target == null || Target.Value == null)
+		{
+			Target.Value = GameObject.FindGameObjectWithTag("Player");
+			Debug.Assert(Target.Value != null, "Could not find Player Object");
+		}
+		m_SpeedManager = Owner.GetComponent<SpeedManager>();
+		Debug.Assert(m_SpeedManager != null, "Could not find speed manager");
+		m_PlayerSpeedManager = Target.Value.GetComponent<SpeedManager>();
+		Debug.Assert(m_PlayerSpeedManager != null, "Could not find speed manager on player");
+	}
+
+	public override void OnStart()
+	{
+		m_Timer = Time.timeSinceLevelLoad + Duration.Value;
+	}
+
+	public override TaskStatus OnUpdate()
+	{
+		if (m_Timer < Time.timeSinceLevelLoad)
+		{
+			return TaskStatus.Success;
+		}
+		if (m_SpeedManager.GetTruePos().x > m_PlayerSpeedManager.GetTruePos().x)
+		{
+			Owner.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+		}
+		else
+		{
+			Owner.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+		}
+		return TaskStatus.Running;
+	}
 
 }
